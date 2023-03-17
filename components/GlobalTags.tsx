59a89651d7e866ae86d422b6c2b77e0d798d7f86
gabriel.strieder <gabriel.strieder@assurant.com>
2023-03-17 20:18:10 -0300

    fix: config files edited

--- components/GlobalTags.tsx
+++ components/GlobalTags.tsx
@@ -6,20 +6,20 @@ function GlobalTags() {
       {/* Icons */}
       <link
         rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href={asset("/favicon-32x32.png")}
+        type="image/ico"
+        sizes="64x64"
+        href={asset("/favicon-64x64.ico")}
       />
       <link
         rel="icon"
-        type="image/png"
+        type="image/ico"
         sizes="16x16"
-        href={asset("/favicon-16x16.png")}
+        href={asset("/favicon-64x64.ico")}
       />
       <link
         rel="shortcut icon"
         type="image/x-icon"
-        href={asset("/favicon-32x32.png")}
+        href={asset("/favicon-64x64.ico")}
       />
 
       {/* Web Manifest */}
@@ -39,70 +39,26 @@ function GlobalTags() {
           __html: `
           /* latin-ext */
           @font-face {
-            font-family: 'Albert Sans';
+            font-family: 'Lato';
             font-style: normal;
             font-weight: 400;
             font-display: swap;
             src: url(${
-            asset("/fonts/i7dOIFdwYjGaAMFtZd_QA1ZVYFeQGQyUV3U.woff2")
-          }) format('woff2');
+            asset("/fonts/Lato/Lato-Regular.ttf")
+          }) format('truetype');
             unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-          }
-          /* latin */
-          @font-face {
-            font-family: 'Albert Sans';
-            font-style: normal;
-            font-weight: 400;
-            font-display: swap;
-            src: url(${
-            asset("/fonts/i7dOIFdwYjGaAMFtZd_QA1ZbYFeQGQyU.woff2")
-          }) format('woff2');
-            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
           }
           /* latin-ext */
           @font-face {
-            font-family: 'Albert Sans';
+            font-family: 'Lato';
             font-style: normal;
-            font-weight: 500;
+            font-weight: 900;
             font-display: swap;
             src: url(${
-            asset("/fonts/i7dOIFdwYjGaAMFtZd_QA1ZVYFeQGQyUV3U.woff2")
-          }) format('woff2');
+            asset("/fonts/Lato/Lato-Black.ttf")
+          }) format('truetype');
             unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-          }
-          /* latin */
-          @font-face {
-            font-family: 'Albert Sans';
-            font-style: normal;
-            font-weight: 500;
-            font-display: swap;
-            src: url(${
-            asset("/fonts/i7dOIFdwYjGaAMFtZd_QA1ZbYFeQGQyU.woff2")
-          }) format('woff2');
-            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-          }
-          /* latin-ext */
-          @font-face {
-            font-family: 'Albert Sans';
-            font-style: normal;
-            font-weight: 700;
-            font-display: swap;
-            src: url(${
-            asset("/fonts/i7dOIFdwYjGaAMFtZd_QA1ZVYFeQGQyUV3U.woff2")
-          }) format('woff2');
-            unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-          }
-          /* latin */
-          @font-face {
-            font-family: 'Albert Sans';
-            font-style: normal;
-            font-weight: 700;
-            font-display: swap;
-            src: url(${
-            asset("/fonts/i7dOIFdwYjGaAMFtZd_QA1ZbYFeQGQyU.woff2")
-          }) format('woff2');
-            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-          }
+          }       
       `,
         }}
       />
