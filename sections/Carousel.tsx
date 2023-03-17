9539189344467b37704f07a535865d6ba6a0b440
Guilherme Canella <guycanella@gmail.com>
2023-03-17 20:33:41 -0300

    first config

--- sections/Carousel.tsx
+++ sections/Carousel.tsx
@@ -1,3 +1,4 @@
 export { default } from "$store/components/ui/BannerCarousel.tsx";
 export type { Banner, Props } from "$store/components/ui/BannerCarousel.tsx";
 export type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+// alguma coisa
