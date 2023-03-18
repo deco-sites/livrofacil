985ef1c0eda7a051528a57b95b6b25cb8d08b70e
Marco Ferreira <mw.marcowilliam@gmail.com>
2023-03-18 19:16:04 -0300

    fix: ajuste modal componentizavel

--- components/header/Modals.tsx
+++ components/header/Modals.tsx
@@ -5,66 +5,104 @@ import { useUI } from "$store/sdk/useUI.ts";
 import type { Props as MenuProps } from "$store/components/header/Menu.tsx";
 import type { Props as SearchbarProps } from "$store/components/search/Searchbar.tsx";
 import Loading from "$store/components/ui/Loading.tsx";
+import Text from "../ui/Text.tsx";
+import Button from "../ui/Button.tsx";
+import Icon from "../ui/Icon.tsx";
 
 const Menu = lazy(() => import("$store/components/header/Menu.tsx"));
 const Cart = lazy(() => import("$store/components/minicart/Cart.tsx"));
 const Searchbar = lazy(() => import("$store/components/search/Searchbar.tsx"));
 
 interface Props {
-  menu: MenuProps;
-  searchbar?: SearchbarProps;
+	menu: MenuProps;
+	searchbar?: SearchbarProps;
 }
 
 function Modals({ menu, searchbar }: Props) {
-  const { displayCart, displayMenu, displaySearchbar } = useUI();
+	const { displayCart, displayMenu, displaySearchbar } = useUI();
 
-  return (
-    <>
-      <Modal
-        title="BEM-VINDO!
-        JÁ É CADASTRADO?"
-        mode="sidebar-left"
-        loading="lazy"
-        open={displayMenu.value}
-        onClose={() => {
-          displayMenu.value = false;
-        }}
-      >
-        <Suspense fallback={<Loading />}>
-          <Menu {...menu} />
-        </Suspense>
-      </Modal>
+	return (
+		<>
+			<Modal
+				// title="BEM-VINDO!
+				// JÁ É CADASTRADO?"
+				mode="sidebar-left"
+				loading="lazy"
+				headerContent={
+					<header class="flex px-4 justify-between  border-b-1 border-default bg-secondaryBlue items-start h-20">
+						<div class="flex flex-col pt-5 pb-5 h-20 relative">
+							<h1>
+								<Text
+									variant="heading-2"
+									class="text-white"
+									tone="default-inverse"
+								>
+									BEM-VINDO!
+								</Text>
+							</h1>
+							<h1>
+								<Text
+									variant="heading-2"
+									class="text-white"
+									tone="default-inverse"
+								>
+									JÁ É CADASTRADO?
+								</Text>
+							</h1>
+						</div>
+						<Button
+							variant="icon"
+							onClick={() => {
+								displayMenu.value = false;
+							}}
+						>
+							<Icon id="XMark" width={20} height={20} strokeWidth={2} />
+						</Button>
+					</header>
+				}
+				open={displayMenu.value}
+				onClose={() => {
+					displayMenu.value = false;
+				}}
+				class="w-2/3"
+			>
+				<Suspense fallback={<Loading />}>
+					<Menu {...menu} />
+				</Suspense>
+			</Modal>
 
-      <Modal
-        title="Buscar"
-        mode="sidebar-right"
-        loading="lazy"
-        open={displaySearchbar.value &&
-          window?.matchMedia("(max-width: 767px)")?.matches}
-        onClose={() => {
-          displaySearchbar.value = false;
-        }}
-      >
-        <Suspense fallback={<Loading />}>
-          <Searchbar {...searchbar} />
-        </Suspense>
-      </Modal>
+			<Modal
+				title="Buscar"
+				mode="sidebar-right"
+				loading="lazy"
+				open={
+					displaySearchbar.value &&
+					window?.matchMedia("(max-width: 767px)")?.matches
+				}
+				onClose={() => {
+					displaySearchbar.value = false;
+				}}
+			>
+				<Suspense fallback={<Loading />}>
+					<Searchbar {...searchbar} />
+				</Suspense>
+			</Modal>
 
-      <Modal
-        title="Minha sacola"
-        mode="sidebar-right"
-        loading="lazy"
-        open={displayCart.value}
-        onClose={() => {
-          displayCart.value = false;
-        }}
-      >
-        <Suspense fallback={<Loading />}>
-          <Cart />
-        </Suspense>
-      </Modal>
-    </>
-  );
+			<Modal
+				title="Minha sacola"
+				mode="sidebar-right"
+				loading="lazy"
+				open={displayCart.value}
+				onClose={() => {
+					displayCart.value = false;
+				}}
+			>
+				<Suspense fallback={<Loading />}>
+					<Cart />
+				</Suspense>
+			</Modal>
+		</>
+	);
 }
 
 export default Modals;
--- components/ui/Modal.tsx
+++ components/ui/Modal.tsx
@@ -3,7 +3,7 @@ import Button from "$store/components/ui/Button.tsx";
 import { useEffect, useRef } from "preact/hooks";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { useSignal } from "@preact/signals";
-import type { JSX } from "preact";
+import type { ComponentChildren, JSX } from "preact";
 
 import Icon from "./Icon.tsx";
 
@@ -19,6 +19,7 @@ export type Props = JSX.IntrinsicElements["dialog"] & {
   mode?: "sidebar-right" | "sidebar-left" | "center";
   onClose?: () => Promise<void> | void;
   loading?: "lazy" | "eager";
+  headerContent?: ComponentChildren 
 };
 
 const styles = {
@@ -34,6 +35,7 @@ const Modal = ({
   onClose,
   children,
   loading,
+  headerContent,
   ...props
 }: Props) => {
   const lazy = useSignal(false);
@@ -66,32 +68,19 @@ const Modal = ({
       onClick={(e) =>
         (e.target as HTMLDialogElement).tagName === "DIALOG" && onClose?.()}
     >
-      <section class=" h-full w-2/3 bg-default flex flex-col ">
-        <header class=" flex px-4 justify-between items-center  border-b-1 border-default bg-secondaryBlue items-start h-20">
-          <div class="flex flex-col pt-5 pb-5">
+      <section class="h-full bg-default flex flex-col">
+        {
+          headerContent ? headerContent : (
+          <header class="flex px-4 justify-between items-center pb-6 border-b-1 border-default">
             <h1>
-              <Text
-                variant="heading-2"
-                class="text-white"
-                tone="default-inverse"
-              >
-                BEM-VINDO!
-              </Text>
+              <Text variant="heading-2">{title}</Text>
             </h1>
-            <h1>
-              <Text
-                variant="heading-2"
-                class="text-white"
-                tone="default-inverse"
-              >
-                JÁ É CADASTRADO?
-              </Text>
-            </h1>
-          </div>
-          <Button variant="icon" onClick={onClose}>
-            <Icon id="XMark" width={20} height={20} strokeWidth={2} />
-          </Button>
-        </header>
+            <Button variant="icon" onClick={onClose}>
+              <Icon id="XMark" width={20} height={20} strokeWidth={2} />
+            </Button>
+          </header>
+          )
+        }
         <div class="overflow-y-auto h-full flex flex-col">
           {loading === "lazy" ? lazy.value && children : children}
         </div>
