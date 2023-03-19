da4b5a9949bd8a18fda3eabe0fd5f7401c109639
Guilherme Canella <guilherme.canella@corebiz.ag>
2023-03-19 11:49:49 -0300

    fix merge conflicts

--- components/ui/BannerCarousel.tsx
+++ components/ui/BannerCarousel.tsx
@@ -73,7 +73,8 @@ function BannerItem({ image, lcp }: { image: Banner; lcp?: boolean }) {
             alt={alt}
           />
         </Picture>
-        {/* {action && (
+        {
+          /* {action && (
           <div
             class="absolute top-0 bottom-0 m-auto left-0 right-0 sm:right-auto sm:left-[12%] max-h-min max-w-[235px] flex flex-col gap-4 bg-hover-inverse p-4 rounded"
             style={{ backdropFilter: "blur(8px)" }}
@@ -86,7 +87,8 @@ function BannerItem({ image, lcp }: { image: Banner; lcp?: boolean }) {
             </Text>
             <Button variant="secondary">{action.label}</Button>
           </div>
-        )} */}
+        )} */
+        }
       </a>
     </div>
   );
